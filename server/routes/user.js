6944aa8163b96d6d70e9128edfebd7d466ad2350
Yar <slvwinswim@gmail.com>
2023-10-20 20:26:17 +0300

    delete user router fixed

--- server/routes/user.js
+++ server/routes/user.js
@@ -26,9 +26,10 @@ router.patch("/:id", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
 
   //DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  router.delete("/:id", async (req, res) => {
     try {
       await User.findByIdAndDelete(req.params.id);
       res.status(200).json("User has been deleted");
@@ -36,11 +37,10 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
       res.status(500).json(err);
     }
   });
-
-});
+  
 
 //GET USER
-router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+router.get("/:id", async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
       const { password, ...others } = user._doc;
@@ -64,7 +64,7 @@ router.get("/", async (req, res) => {
   });
 
   //GET USER STATS
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+router.get("/stats", async (req, res) => {
     const date = new Date();
     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
     try {

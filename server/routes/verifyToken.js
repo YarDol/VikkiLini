5449c55dc5df4f4c4dc7723e4499608a0ed3067e
YarDol <yardolhushyn@gmail.com>
2024-06-24 00:48:33 +0300

    chore: Update verifyToken.js to include verifyTokenAndAuthorization function

--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -13,6 +13,7 @@ const verifyToken = (req, res, next) => {
     return res.status(401).json("You are not authenticated.");
   }
 };
+//
 
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {

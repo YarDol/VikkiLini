f6405afa8bc71bb6bb608201a16aad65a8fe0e51
YarDol <yardolhushyn@gmail.com>
2024-06-24 00:55:38 +0300

    chore: Update i18n configuration for language detection and backend loading
    Refactor the i18n configuration in the index.js file to improve language detection and backend loading. This includes updating the imports for the LanguageDetector and HttpApi modules, as well as configuring the supported languages and fallback language. Additionally, the load path for the translation files has been updated to match the new directory structure.

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,9 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
-import { createRoot } from 'react-dom/client';
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import { createRoot } from "react-dom/client";
 import styled from "styled-components";
 import Spinner from "./utility/spinner";
 
@@ -18,32 +18,38 @@ const Container = styled.div`
 
 //Localisaton
 i18n
-.use(HttpApi)
-.use(initReactI18next) // passes i18n down to react-i18next
-.use(LanguageDetector)
-.init({
-  supportedLngs: ['en','ua'],
-  fallbackLng: "en",
-  detection: {
-    order: ['localStorage', 'htmlTag', 'path', 'subdomain'],
-    caches: ['localStorage', 'cookie']
-  },
-  backend: {
-   loadPath: '/assets/locales/{{lng}}/translation.json',
-  },
-  
+  .use(HttpApi)
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .init({
+    supportedLngs: ["en", "ua"],
+    fallbackLng: "en",
+    detection: {
+      order: ["localStorage", "htmlTag", "path", "subdomain"],
+      caches: ["localStorage", "cookie"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
 
-});
+const root = createRoot(document.getElementById("root"));
 
-const root = createRoot(document.getElementById('root'));
+console.log(process.env);
 
 root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <Suspense fallback={<div><Spinner/></div>}>
-      <Container>
-        <App />
-      </Container>
+      <Suspense
+        fallback={
+          <div>
+            <Spinner />
+          </div>
+        }
+      >
+        <Container>
+          <App />
+        </Container>
       </Suspense>
     </PersistGate>
   </Provider>
